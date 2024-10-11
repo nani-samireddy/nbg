9d40f438e31287fc18a682e7421b6539e3e08bf5
nani-samireddy <nanisamireddy05@gmail.com>
2024-10-11 15:56:52 +0530

    Fix routing

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -1,35 +1,34 @@
 import Image from "next/image";
-import logo2 from './logo.svg'
+import Link from "next/link";
+import logo2 from './logo.svg';
+
 export default function Navbar() {
     return (
         <nav className="flex items-center justify-between px-[4%] md:px-[10%] py-10">
-            <a href="/">
-                <Image
-                    aria-hidden
-                    src={logo2}
-                    alt="Logo"
-                    width={100}
-                    height={100}
-                /></a>
+            <Link href="/" passHref>
+                
+                    <Image
+                        aria-hidden
+                        src={logo2}
+                        alt="Logo"
+                        width={100}
+                        height={100}
+                    />
+                
+            </Link>
 
             <ul className="flex items-center gap-1 mx-6">
                 <li className="hover:bg-slate-200 px-6 py-1 rounded-md transition-all duration-200">
-                    <a
-                        href="/"
-                        className="text-lg "
-                    >
-                        Songs
-                    </a>
+                    <Link href="/" passHref>
+                        <span className="text-lg">Songs</span>
+                    </Link>
                 </li>
                 <li className="hover:bg-slate-200 px-6 py-1 rounded-md transition-all duration-200">
-                    <a
-                        href="/about"
-                        className="text-lg "
-                    >
-                        About
-                    </a>
+                    <Link href="/about" passHref>
+                        <span className="text-lg">About</span>
+                    </Link>
                 </li>
             </ul>
         </nav>
-    )
+    );
 }
