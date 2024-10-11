60dc24d7027d221fea6a5e6ce26bab6da14d4509
nani-samireddy <nanisamireddy05@gmail.com>
2024-10-11 16:01:10 +0530

    fix

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -5,8 +5,7 @@ import logo2 from './logo.svg';
 export default function Navbar() {
     return (
         <nav className="flex items-center justify-between px-[4%] md:px-[10%] py-10">
-            <Link href="/" passHref>
-                
+            <Link href="/">
                     <Image
                         aria-hidden
                         src={logo2}
@@ -19,12 +18,12 @@ export default function Navbar() {
 
             <ul className="flex items-center gap-1 mx-6">
                 <li className="hover:bg-slate-200 px-6 py-1 rounded-md transition-all duration-200">
-                    <Link href="/" passHref>
+                    <Link href="/" >
                         <span className="text-lg">Songs</span>
                     </Link>
                 </li>
                 <li className="hover:bg-slate-200 px-6 py-1 rounded-md transition-all duration-200">
-                    <Link href="/about" passHref>
+                    <Link href="/about" >
                         <span className="text-lg">About</span>
                     </Link>
                 </li>
